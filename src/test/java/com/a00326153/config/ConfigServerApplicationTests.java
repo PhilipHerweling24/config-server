bed853278e71f7db9d7aabfc7c11b101247a8b69
PhilipHerweling <miggymorette@gmail.com>
2025-04-05 13:58:10 +0100

    Initialised with spring initializr

---
+++ src/test/java/com/a00326153/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a00326153.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
